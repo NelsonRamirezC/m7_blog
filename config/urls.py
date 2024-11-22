9b47a6ce026c2f3146afc7f82b770b5e0557a585
Nelson Ramirez <xhyskoax@gmail.com>
2024-11-22 20:04:03 -0300

    se deja funcionando el proyecto para comenzar

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls'))
+    path('', views.index, name="index"),
+    path('', include('usuarios.urls')),
+    # path('blog/', include('blog.urls')),
 ]
